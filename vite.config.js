21dee8bf57cf0526252568458f2d3ebd434b831a
GeniuX <geniuh7@gmail.com>
2023-11-22 22:00:43 -0800

    commmit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+    base:"http://GeniuX-arch.github.io/paginaweb-administrativa"
+  })
